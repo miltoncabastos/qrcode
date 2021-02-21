92679fc925be8e5820efe7e15c07e60369755dd8
milton.bastos <milton.bastos@ambevtech.com.br>
2021-02-21 18:13:21 -0300

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+using Microsoft.Extensions.Logging;
+
+namespace QrWeb.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public byte[] Image;
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Image = QrCoderGenerator.GenerateByteArray("https://www.linkedin.com/in/miltoncabastos/");
+        }
+    }
+}
